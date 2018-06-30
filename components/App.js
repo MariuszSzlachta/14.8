67435310ceb6cf0f3964e02817a3469c4f91ee4b
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-06-30 17:54:02 +0200

    Complete task and add comments

--- components/App.js
+++ components/App.js
@@ -1,16 +1,23 @@
 App = React.createClass({
 	getInitialState() {
+		// ustawienie stanu:
+		// loading false - nic nie ładujemy
+		// searchingText - nic nie pobraliśmy z komponentu search
+		// gif - w związku z tym git to pusty obiekt
 		return {
 			loading: false,
 			searchingText: '',
 			gif: {}
 		}
 	},
-
+	
 	handleSearch: function(searchingText) {
+		// obsługuje komunikację z inputem w komponencie search
+		// stan na true bo jak uruchomiliśmy funkcję, to wiadomo, że coś pobieramy
 		this.setState({
 			loading: true
 		});
+		// na podstawie textu pobranego z inputa 
 		this.getGif(searchingText, function(gif) {
 			this.setState({
 				loading: false,
@@ -22,21 +29,23 @@ App = React.createClass({
 
 	getGif: function(searchingText, callback) {
 		var GIPHY_PUB_KEY = 'qYJUoExqhocza5OQXzJpzFgXT2AxuaGW';
-    	var GIPHY_API_URL = 'https://api.giphy.com';
+    var GIPHY_API_URL = 'https://api.giphy.com';
 		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText; 
-    	var xhr = new XMLHttpRequest();
-    	xhr.open('GET', url);
-    	xhr.onload = function() {
-        	if (xhr.status === 200) {
-        		var data = JSON.parse(xhr.responseText).data; 
-        		var gif = { 
-        			url: data.fixed_width_downsampled_url,
-        			sourceUrl: data.url
-        		};
-            	callback(gif); 
-        	}
-    	};
-    	xhr.send();
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+
+		xhr.onload = function() {
+			if (xhr.status === 200) {
+				var data = JSON.parse(xhr.responseText).data; 
+				var gif = { 
+					url: data.fixed_width_downsampled_url,
+					sourceUrl: data.url
+				};
+			// po co ten callback? Czy to jest odwołanie do funkcji z 21 linijki? Czy chodzi o to, że w ten sposób przekazujemy info to setState ?
+			callback(gif); 
+			}
+		};
+    xhr.send();
 	},
 
 	render: function() {
--- components/Search.js
+++ components/Search.js
@@ -1,23 +1,29 @@
 Search = React.createClass({
   getInitialState() {
+    // funkcja inicjująca stan zwraca klucz serchingText, na początku oczywiście nic nie ma w nim bo jeszcze nic nie wpisaliśmy więc pusty string
     return {
       searchingText: ''
     };
   },
 
   handleChange: function (event) {
+    // do zmiennej searching text przypisz value inputa
     var searchingText = event.target.value;
+    // by wymuśić przerenderowanie elementu trzeba zmienić mu stan więc przypisujemy to co pobraliśmy do searchingText w stanie komponentu
     this.setState({
       searchingText: searchingText
     });
-
+    // jeśli wpisywany text jest dłuższy niż dwa znaki to uruchom funkcję onSearch przekazaną jako props od rodzica i jako parametr podaj jej searchingText, można wpisać zwyczajnie bo jest to zmienna w tej samej funkcji patrz wyżej
     if (searchingText.length > 2) {
       this.props.onSearch(searchingText);
     }
   },
 
   handleKeyUp: function (event) {
+    // po zwolnieniu klawisza sprawdza czy to enter jeśli tak to odpala funkcje onSearch podaną w propsach jest to funkcja handleSearch w komponencie app
     if (event.keyCode === 13) {
+      // tutaj jako parametr już trzeba podać searchingText z this.state czyli ? this odnosi się do instancji komponentu czy do handleSearch bo był bind?  PYTANKO!!
+      // PYTANKO 2: czy jeśli funkcją coś zwraca to to co zwraca onSearch będzie zwrócone do App gdzie jest zadeklarowana? Stąd mamy info w app o wyszukiwanym tekście?
       this.props.onSearch(this.state.searchingText);
     }
   },
@@ -30,20 +36,20 @@ Search = React.createClass({
     };
 
     return <input
-    type = "text"
-    onChange = {
-      this.handleChange
-    }
-    onKeyUp = {
-      this.handleKeyUp
-    }
-    placeholder = "Tutaj wpisz wyszukiwaną frazę"
-    style = {
-      styles
-    }
-    value = {
-      this.state.searchTerm
-    }
-    />
+      type = "text"
+      onChange = {
+        this.handleChange
+      }
+      onKeyUp = {
+        this.handleKeyUp
+      }
+      placeholder = "Tutaj wpisz wyszukiwaną frazę"
+      style = {
+        styles
+      }
+      value = {
+        this.state.searchTerm
+      }
+      />
   }
 });
