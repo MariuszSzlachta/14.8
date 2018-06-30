7226b6938e0bba715d2f4c490ad6996135197218
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-06-30 16:28:19 +0200

    Release candidate

---
+++ components/Search.js
@@ -0,0 +1,33 @@
+Search = React.createClass({
+  getInitialState() {
+    return {
+      searchingText: ''
+    }
+  },
+
+  handleChange: function(event){
+    var searchingText = event.target.value;
+    this.setState({
+      searchingText: searchingText
+    });
+    if (searchingText > 2){
+      this.props.onSearch(searchingText);
+    }
+  },
+
+  handleKeyUp: function(event){
+    if (event.keyCode === 13){
+      this.props.onKeyUp(this.state.searchingText);
+    }
+  },
+
+  render: function(){
+    var styles = {
+      fontSize = '1.5em',
+      width: '90%',
+      maxWidth: '350px'
+    };
+
+    return <input type='text' onChange={this.handleChange} onKeyUp={this.handleKeyUp} placeholder='Tu wpisz wyszukiwaną frazę' style={styles} value={this.state.searchTerm} />
+  }
+});
