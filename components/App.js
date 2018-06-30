5e1fc2dbe45cd2e936601871abd20e000af17c3c
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-06-30 17:01:46 +0200

    Rewrite all

--- components/App.js
+++ components/App.js
@@ -1,64 +1,64 @@
-var GIPHY_API_URL = 'https://api.giphy.com';
-var GIPHY_PUB_KEY = 'q4k1xiFfyXHFEVGeG4SYB8VJsIA5OtQu';
 
 App = React.createClass({
-  getInitialState() {
-    return {
-      loading: false,
-      searchingText: '',
-      gif: {}
-    }
-  },
+	getInitialState() {
+		return {
+			loading: false,
+			searchingText: '',
+			gif: {}
+		}
+	},
 
-  handleSearch: function(searchingText){
-    this.setState({
-      loading: true
-    });
+	handleSearch: function(searchingText) {
+		this.setState({
+			loading: true
+		});
+		this.getGif(searchingText, function(gif) {
+			this.setState({
+				loading: false,
+				gif: gif,
+				searchingText: searchingText
+			});
+		}.bind(this));
+	},
 
-    this.getGif(searchingText, function(gif){
-      this.setState({
-        loading: false,
-        gif: gif,
-        searchingText: searchingText
-      });
-    }.bind(this));
-  },
+	getGif: function(searchingText, callback) {
+		var GIPHY_PUB_KEY = 'qYJUoExqhocza5OQXzJpzFgXT2AxuaGW';
+    	var GIPHY_API_URL = 'https://api.giphy.com';
+		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText; 
+    	var xhr = new XMLHttpRequest();
+    	xhr.open('GET', url);
+    	xhr.onload = function() {
+        	if (xhr.status === 200) {
+        		var data = JSON.parse(xhr.responseText).data; 
+        		var gif = { 
+        			url: data.fixed_width_downsampled_url,
+        			sourceUrl: data.url
+        		};
+            	callback(gif); 
+        	}
+    	};
+    	xhr.send();
+	},
 
-  getGif : function(searchingText, callback){
-    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-    var xhr = new XMLHttpRequest()
-    xhr.open('GET', url);
-    xhr.onload = function(){
-      if (xhr.status === 200){
-        var data = JSON.parse(xhr.responseText).data;
-        var gif = {
-          url: data.fixed_width_downsampled_url,
-          sourceUrl: data.url
-        };
-        callback(gif);
-      }
-    };
-    xhr.send();
-  },
+	render: function() {
 
-  render: function(){
-    var styles = {
-      margin: '0 auto',
-      textAlign: 'center',
-      width: '90%'
-    };
-    
-    return (
-      <div style={styles}>
-        <h1>Wyszukiwarka Gifów</h1>
-        <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter by pobrać kolejne gify</p>
-        <Search onSearch={this.handleSearch} />
-        <Gif
-          loading={this.state.loading}
-          url={this.state.gif.url}
-          sourceUrl={this.state.gif.sourceUrl}
-        />
-      </div>
-    );
-  }
+		var styles = {
+			margin: '0 auto',
+			textAligin: 'center',
+			width: '90%'
+		};
+
+		return (
+			<div style={styles}>
+				<h1>Wyszukiwarka GIFów</h1>
+				<p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+				<Search onSearch={this.handleSearch}/>
+				<Gif 
+				loading={this.state.loading}
+				url={this.state.gif.url}
+				sourceUrl={this.state.gif.sourceUrl}
+				/>
+			</div>
+		);
+	}
 });
--- components/Search.js
+++ components/Search.js
@@ -1,33 +1,35 @@
 Search = React.createClass({
-  getInitialState() {
-    return {
-      searchingText: ''
-    }
-  },
+	getInitialState() {
+		return {
+			searchingText: ''
+		};
+	},
 
-  handleChange: function(event){
-    var searchingText = event.target.value;
-    this.setState({
-      searchingText: searchingText
-    });
-    if (searchingText > 2){
-      this.props.onSearch(searchingText);
-    }
-  },
+	handleChange: function(event) {
+		var searchingText = event.target.value;
+		this.setState({searchingText: searchingText});
 
-  handleKeyUp: function(event){
-    if (event.keyCode === 13){
-      this.props.onSearch(this.state.searchingText);
-    }
-  },
+		if (searchingText.length > 2) {
+			this.props.onSearch(searchingText);
+		}
+	},
 
-  render: function(){
-    var styles = {
-      fontSize: '1.5em',
-      width: '90%',
-      maxWidth: '350px'
-    };
+	handleKeyUp: function(event) {
+		if (event.keyCode === 13) {
+			this.props.onSearch(this.state.searchingText);
+		}
+	},
+	
+	render: function() {
+		var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
 
-    return <input type='text' onChange={this.handleChange} onKeyUp={this.handleKeyUp} placeholder='Tu wpisz wyszukiwaną frazę' style={styles} value={this.state.searchTerm} />
-  }
+	return <input
+				type="text"
+				onChange={this.handleChange}
+				onKeyUp={this.handleKeyUp}
+				placeholder="Tutaj wpisz wyszukiwaną frazę"
+				style={styles}
+				value={this.state.searchTerm}
+			/>
+	}
 });
